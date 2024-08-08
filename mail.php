b978f03e93b0b18a172c2f1312d52ef61f636659
HuangHao0116 <hhuang95-c@my.cityu.edu.hk>
2024-08-08 23:25:52 +0800

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,18 +1,18 @@
 <?php
 
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+  $name =$_POST['name'];
+  $email =$_POST['email'];
+  $subject =$_POST['subject'];
+  $message =$_POST['message'];
 
   $mailheader = "From:".$name."<".$email.">\r\n";
-  
+
   $recipient = "hhuang95-c@my.cityu.edu.hk";
 
-  mail($recipient, $subject, $message, $mailheaders)
-  or die("Error!");
-  
+// 注意这里修正了变量名，使用了 $mailheader 而不是$mailheaders
+  mail($recipient,$subject, $message,$mailheader) 
+  or die("Error sending email!");
 
-  echo "message send";
+  echo "Message sent"; // 这里应该是 "Message sent"，为了保持语句的准确性
 
 ?>
